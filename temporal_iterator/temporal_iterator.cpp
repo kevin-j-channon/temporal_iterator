6a6a9933e2d44d60d303a6db9b7d086deb2a68a4
kevin <kevinchannon@gmail.com>
2022-02-15 18:55:57 +0000

    Add project files.

---
+++ temporal_iterator/temporal_iterator.cpp
@@ -0,0 +1,11 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test_temporal_iterator
+{
+	TEST_CLASS(TestConstruction)
+	{
+	public:
+	};
+}
